56ec95c50431796a1aab230ea8b058076cee4773
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-18 12:51:39 +0330

    add static and interface polymrphism

---
+++ DesignPatterns/Basics/InterfacePolymorphism.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Basics
+{
+    public interface IPetFood
+    {
+        int Energy { get; }
+    }
+    public class Kibble : IPetFood
+    {
+        public int Energy => 7;
+    }
+    public class Fish : IPetFood
+    {
+        int IPetFood.Energy => 8;
+    }
+}
---
+++ DesignPatterns/Basics/StaticPolymorphism.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Basics
+{
+    internal class StaticPolymorphism
+    {
+        public void Feed(PetDog dog)
+        {
+            PetFeeder.FeedPet(dog, new Kibble());
+        }
+        public void Feed(PetCat cat)
+        {
+            PetFeeder.FeedPet(dog, new Fish());
+        }
+    }
+}
