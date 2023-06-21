3c3c468c92472e24780ce2977715f7dbf4c0a7ba
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-22 01:34:43 +0330

    interface polymorphism and generic methods added

---
+++ DesignPatterns/Basics/GenericClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Basics
+{
+    // pet feeder
+    public static class GenericClass
+    {
+        public static void FeedPet<TP, TF>(TP pet, TF food) where TP : PetAnimal where TF : IPetFood
+        {
+            pet.Feed(food);
+        }
+    }
+}
--- DesignPatterns/Basics/InterfacePolymorphism.cs
+++ DesignPatterns/Basics/InterfacePolymorphism.cs
@@ -18,4 +18,5 @@ namespace DesignPatterns.Basics
     {
         int IPetFood.Energy => 8;
     }
+
 }
