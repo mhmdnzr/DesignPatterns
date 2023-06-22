933da0e9ee5105178cbdb48807ad796212d563a5
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-22 08:21:23 +0330

    implement soft example

--- RealWorldExamples/src/creational/FactoryMethod/Conceptual.cs
+++ RealWorldExamples/src/creational/FactoryMethod/Conceptual.cs
@@ -2,77 +2,77 @@
 
 namespace src.creational.FactoryMethod
 {
-    // The Creator class declares the factory method that is supposed to return
-    // an object of a Product class. The Creator's subclasses usually provide
+    // The Dialog class declares the factory method that is supposed to return
+    // an object of a Product class. The Dialog's subclasses usually provide
     // the implementation of this method.
-    abstract class Creator
+    abstract class Dialog
     {
-        // Note that the Creator may also provide some default implementation of
+        // Note that the Dialog may also provide some default implementation of
         // the factory method.
-        public abstract IProduct FactoryMethod();
+        public abstract IButton FactoryMethod();
 
-        // Also note that, despite its name, the Creator's primary
+        // Also note that, despite its name, the Dialog's primary
         // responsibility is not creating products. Usually, it contains some
         // core business logic that relies on Product objects, returned by the
         // factory method. Subclasses can indirectly change that business logic
         // by overriding the factory method and returning a different type of
         // product from it.
-        public string SomeOperation()
+        public string RenderButton()
         {
             // Call the factory method to create a Product object.
             var product = FactoryMethod();
             // Now, use the product.
-            var result = "Creator: The same creator's code has just worked with "
-                + product.Operation();
+            var result = "Dialog: The same Dialog's code has just worked with "
+                + product.OnClick();
 
             return result;
         }
     }
 
-    // Concrete Creators override the factory method in order to change the
+    // Concrete Dialogs override the factory method in order to change the
     // resulting product's type.
-    class ConcreteCreator1 : Creator
+    class ConcreteDialog1 : Dialog
     {
         // Note that the signature of the method still uses the abstract product
         // type, even though the concrete product is actually returned from the
-        // method. This way the Creator can stay independent of concrete product
+        // method. This way the Dialog can stay independent of concrete product
         // classes.
-        public override IProduct FactoryMethod()
+        public override IButton FactoryMethod()
         {
-            return new ConcreteProduct1();
+            return new WinButton();
         }
     }
 
-    class ConcreteCreator2 : Creator
+    class ConcreteDialog2 : Dialog
     {
-        public override IProduct FactoryMethod()
+        public override IButton FactoryMethod()
         {
-            return new ConcreteProduct2();
+            return new WebButton();
         }
     }
 
-    // The Product interface declares the operations that all concrete products
+    // The Product interface declares the OnClicks that all concrete products
     // must implement.
-    public interface IProduct
+    public interface IButton
     {
-        string Operation();
+        string OnClick();
     }
 
     // Concrete Products provide various implementations of the Product
     // interface.
-    class ConcreteProduct1 : IProduct
+    class WinButton : IButton
     {
-        public string Operation()
+        public string OnClick()
         {
-            return "{Result of ConcreteProduct1}";
+            return "{Result of WinButton}";
         }
     }
 
-    class ConcreteProduct2 : IProduct
+    class WebButton : IButton
     {
-        public string Operation()
+        public string OnClick()
         {
-            return "{Result of ConcreteProduct2}";
+            return "{Result of WebButton}";
         }
     }
 
@@ -80,24 +80,24 @@ namespace src.creational.FactoryMethod
     {
         public void Main()
         {
-            Console.WriteLine("App: Launched with the ConcreteCreator1.");
-            ClientCode(new ConcreteCreator1());
+            Console.WriteLine("App: Launched with the ConcreteDialog1.");
+            ClientCode(new ConcreteDialog1());
 
             Console.WriteLine("");
 
-            Console.WriteLine("App: Launched with the ConcreteCreator2.");
-            ClientCode(new ConcreteCreator2());
+            Console.WriteLine("App: Launched with the ConcreteDialog2.");
+            ClientCode(new ConcreteDialog2());
         }
 
-        // The client code works with an instance of a concrete creator, albeit
+        // The client code works with an instance of a concrete Dialog, albeit
         // through its base interface. As long as the client keeps working with
-        // the creator via the base interface, you can pass it any creator's
+        // the Dialog via the base interface, you can pass it any Dialog's
         // subclass.
-        public void ClientCode(Creator creator)
+        public void ClientCode(Dialog Dialog)
         {
             // ...
-            Console.WriteLine("Client: I'm not aware of the creator's class," +
-                "but it still works.\n" + creator.SomeOperation());
+            Console.WriteLine("Client: I'm not aware of the Dialog's class," +
+                "but it still works.\n" + Dialog.RenderButton());
             // ...
         }
     }
