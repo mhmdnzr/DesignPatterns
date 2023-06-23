b4b5411e19a9f77b3ef6f52606123bda5ef79b24
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-23 18:04:04 +0330

    builder dp implemented

--- RealWorldExamples/Program.cs
+++ RealWorldExamples/Program.cs
@@ -1,11 +1,11 @@
 ﻿// call app there
 using RealWorldExamples.src.creational.AbstractFactory;
+using RealWorldExamples.src.creational.Builder;
 using RealWorldExamples.src.creational.FactoryMethod;
 
 
 Console.WriteLine("Hello, World!");
 
 //RunFactoryMethodProgram.Main();
-
-
-RunAbstractFactoryProgram.Main();
+//RunAbstractFactoryProgram.Main();
+RunBuilderProgram.Main();
