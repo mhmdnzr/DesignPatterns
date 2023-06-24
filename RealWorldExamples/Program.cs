5345e298dd99006f3cb7d80a628ec03e65bb3923
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-24 10:38:07 +0330

    implement prototype pattern

--- RealWorldExamples/Program.cs
+++ RealWorldExamples/Program.cs
@@ -2,10 +2,11 @@
 using RealWorldExamples.src.creational.AbstractFactory;
 using RealWorldExamples.src.creational.Builder;
 using RealWorldExamples.src.creational.FactoryMethod;
-
+using RealWorldExamples.src.creational.Prototype;
 
 Console.WriteLine("Hello, World!");
 
 //RunFactoryMethodProgram.Main();
 //RunAbstractFactoryProgram.Main();
-RunBuilderProgram.Main();
+//RunBuilderProgram.Main();
+RunPrototypeProgram.Main();
