4cd08e85c069f9082634708fc9a43641c057d05e
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-24 13:14:49 +0330

    implement singleton patterns

--- RealWorldExamples/Program.cs
+++ RealWorldExamples/Program.cs
@@ -3,10 +3,14 @@ using RealWorldExamples.src.creational.AbstractFactory;
 using RealWorldExamples.src.creational.Builder;
 using RealWorldExamples.src.creational.FactoryMethod;
 using RealWorldExamples.src.creational.Prototype;
+using RealWorldExamples.src.creational.Singleton.Naïve_Singleton;
+using RealWorldExamples.src.creational.Singleton.ThreadSafe_Singleton;
 
 Console.WriteLine("Hello, World!");
 
 //RunFactoryMethodProgram.Main();
 //RunAbstractFactoryProgram.Main();
 //RunBuilderProgram.Main();
-RunPrototypeProgram.Main();
+//RunPrototypeProgram.Main();
+//RunNaïveSingletonProgram.Main();
+RunThreadSafeProgram.Main();
