9d59e0d5a9494806b46c2e22d51b0d1431be8a6a
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-06-24 14:40:32 +0330

    adapter design patter implemented

--- RealWorldExamples/Program.cs
+++ RealWorldExamples/Program.cs
@@ -1,16 +1,31 @@
 ﻿// call app there
+
+/* start creational design pattern */
 using RealWorldExamples.src.creational.AbstractFactory;
 using RealWorldExamples.src.creational.Builder;
 using RealWorldExamples.src.creational.FactoryMethod;
 using RealWorldExamples.src.creational.Prototype;
 using RealWorldExamples.src.creational.Singleton.Naïve_Singleton;
 using RealWorldExamples.src.creational.Singleton.ThreadSafe_Singleton;
+using RealWorldExamples.src.structural.Adapter;
+/* end creational design pattern */
+
+
+
 
 Console.WriteLine("Hello, World!");
-
+/*
+ Creational Design Pattern
+ */
 //RunFactoryMethodProgram.Main();
 //RunAbstractFactoryProgram.Main();
 //RunBuilderProgram.Main();
 //RunPrototypeProgram.Main();
 //RunNaïveSingletonProgram.Main();
-RunThreadSafeProgram.Main();
+//RunThreadSafeProgram.Main();
+
+
+/*
+ Structual Design Pattern
+ */
+RunAdapterProgram.Main();
---
+++ RealWorldExamples/src/structural/Adapter/Structual.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldExamples.src.structural.Adapter
+{
+    /// <summary>
+    /// The 'Target' class
+    /// </summary>
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+    /// <summary>
+    /// The 'Adapter' class
+    /// </summary>
+    public class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+        public override void Request()
+        {
+            // Possibly do some other work
+            // and then call SpecificRequest
+            adaptee.SpecificRequest();
+        }
+    }
+    /// <summary>
+    /// The 'Adaptee' class
+    /// </summary>
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+
+
+    /// <summary>
+    /// Adapter Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create adapter and place a request
+            Target target = new Adapter();
+            target.Request();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
